cc90453cca9db0a9c30440466370038651aa8b60
Anjay Krishnan <151697110+anjk15@users.noreply.github.com>
2025-06-15 10:38:53 -0500

    Model creation

--- Main.py
+++ Main.py
@@ -4,6 +4,9 @@ from nltk.corpus import stopwords
 import nltk
 from sklearn.datasets import fetch_openml
 from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
 
 # Load the dataset
@@ -25,5 +28,18 @@ X_tfidf = vectorizer.fit_transform(X)
 # Convert to PyTorch tensor
 email_tensor = torch.tensor(X_tfidf.toarray(), dtype=torch.float32)
 
+class SpamClassifier(nn.Module):
+    def __init__(self):
+        super(SpamClassifier, self).__init__()
+        self.L1 = nn.Linear(1000, 512)
+        self.L2 = nn.Linear(512, 128)
+        self.L3 = nn.Linear(128, 1)
 
+    def forward(self, x):
+        x = F.relu(self.L1(x))
+        x = F.relu(self.L2(x))
+        x = torch.sigmoid(self.L3(x))  
+        return x
+
+        
 
